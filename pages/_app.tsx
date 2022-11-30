1b51ce57187404552d0afa46214fcdddeab5b05e
L <luigi.gottardello.fonseca@gmail.com>
2022-11-29 22:34:44 -0300

    style: font family globally set up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css';
+import { Poppins } from '@next/font/google';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+
+const fontFamily = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '600', '700', '800'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={fontFamily.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
